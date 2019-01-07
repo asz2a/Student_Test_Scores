cc1ace1b75e8b392377b0feee98b84491d8d6560
Arol Zague <arolzague@Arols-MacBook-Pro.local>
2019-01-06 21:46:49 -0600

    first commit

--- basic.py
+++ basic.py
@@ -19,4 +19,42 @@ def scatterplot():
     plt.plot(xy, xy, color='red')
     plt.show()
 
-scatterplot()
+
+def barchart_ethnicity_scores():
+	# GROUPS TO SCORES
+
+	n_groups = 5
+	index = np.arange(n_groups)
+	bar_width = 0.25
+	fig, ax = plt.subplots()
+
+	math = []
+	reading = []
+	writing = []
+
+	groups = ['group A', 'group B', 'group C', 'group D', 'group E',]
+
+	for group in groups:
+		is_g = df['race/ethnicity'] == group
+		group_df = df[is_g]
+		group_math = group_df['math score'].mean()
+		math.append(group_math)
+		group_read = group_df['reading score'].mean()
+		reading.append(group_read)
+		group_write = group_df['writing score'].mean()
+		writing.append(group_write)
+
+	math_bar = plt.bar(index, math, bar_width, color='r', label='math')
+	reading_bar = plt.bar(index + bar_width, reading, bar_width, color='b', label='reading')
+	writing_bar = plt.bar(index + bar_width*2, writing, bar_width, color='g', label='writing')
+
+	plt.xticks(index + bar_width, groups)
+	plt.ylim(bottom=60)
+	plt.legend()
+	plt.tight_layout()
+
+	plt.show()
+
+
+scatter_plot()
+barchart_ethnicity_scores()
