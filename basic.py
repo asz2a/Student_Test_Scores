8fd6c60fe8e78a2951c5921fcaa7197e4edf7411
Arol Zague <arolzague@Arols-MacBook-Pro.local>
2019-01-06 21:11:53 -0600

    created scatter plot

--- basic.py
+++ basic.py
@@ -2,35 +2,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 df = pd.read_csv("StudentsPerformance.csv", sep=',')
-is_groupb = df['race/ethnicity'] == 'group B'
-is_groupc = df['race/ethnicity'] == 'group C'
-is_groupd = df['race/ethnicity'] == 'group D'
-is_groupe = df['race/ethnicity'] == 'group E'
-is_groupa = df['race/ethnicity'] == 'group A'
 
 
+# Scatter Plot
+def scatterplot():
+    math_score = df['math score']
+    reading_score = df['reading score']
 
-group_B = df[is_groupb]
-group_C = df[is_groupc]
-group_D = df[is_groupd]
-group_E = df[is_groupe]
-group_A = df[is_groupa]
+    y = math_score
+    x = reading_score
 
-averageB = group_B.mean()
-averageC = group_C.mean()
-averageD = group_D.mean()
-averageE = group_E.mean()
-averageA = group_A.mean()
+    plt.scatter(x, y, color='blue')
+    xy= range(0, 100)
+    plt.plot(xy, xy, color='red')
+    plt.show()
 
-
-
-y = np.concatenate((averageB, averageC, averageD, averageE, averageA))
-width = 1/1.5
-x = ['Group B', 'Group C', 'Group D', 'Group E','Group A', 'Group B', 'Group C', 'Group D', 'Group E','Group A', 'Group B', 'Group C', 'Group D', 'Group E','Group A']
-
-plt.bar(x, y, color="blue")
-#
-plt.show()
-
-print(y)
+scatterplot()
